0a4f76a22b49b6ed768f575595fd1cb5e1645e78
liuguihua <liuguihua@wanxue.cn>
2021-06-10 11:03:22 +0800

    change:添加个人中心页面

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,18 +1,39 @@
 // pages/user/user.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    }
+  },
 
+  getUserInfo() {
+    wx.getUserInfo({
+      success: res => {
+        app.globalData.userInfo = res.userInfo
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+        console.log(this.data.userInfo);
+      }
+    })
   },
 
   /**
